9ea7b16b2e4a5c304186f8604c71030173baa736
saliha54 <51030929+saliha54@users.noreply.github.com>
2019-08-11 09:09:49 +0200

    Update yodize.js

--- week-1/fetching-exercises/yodize.js
+++ week-1/fetching-exercises/yodize.js
@@ -1,18 +1,20 @@
 // docs: https://funtranslations.com/api/yoda
 
 function yodize() {
-
-  const phrase = document.getElementById("to-yodize").value;
+  const phrase = document.getElementById('to-yodize').value;
 
   const encoded = encodeURIComponent(phrase);
 
-  const url = "https://api.funtranslations.com/translate/yoda.json?text="+encoded;
+  const url = 'https://api.funtranslations.com/translate/yoda.json?text=' + encoded;
 
   /*
     use fetch to send a get request to the url above
     then log only the translated phrase
   */
-
+  fetch(url).then(function(response) {
+    console.log(response.json());
+    console.log(response.json()); //.content.translated
+  });
 }
 
-document.getElementById("yodize").addEventListener("click", yodize);
+document.getElementById('yodize').addEventListener('click', yodize);
