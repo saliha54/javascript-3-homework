da951577888f139e7d68efc4763202920cc04dab
saliha54 <51030929+saliha54@users.noreply.github.com>
2019-08-11 09:07:12 +0200

    Update reverse.js

--- week-1/three-layer-handlers/reverse.js
+++ week-1/three-layer-handlers/reverse.js
@@ -1,23 +1,24 @@
 function reverse_string(str) {
-  return str;
+  return str
+    .split('')
+    .reverse()
+    .join('');
 }
 
-function reverse_handler() {                                      
-                     
+function reverse_handler() {
   // read and process user input
-  var to_reverse = document.getElementById('to-reverse').value;        
-                                 
+  var to_reverse = document.getElementById('to-reverse').value;
+
   // pass user input through core logic
-  
+
   /* call your logic function to reverse a string */
   /* assign the result to a variable named 'reversed' */
   var reversed = reverse_string(to_reverse);
 
-
   // report result to user
   var output_field = document.getElementById('reversed');
   output_field.innerHTML = reversed;
-};
+}
 
-var reverse_button = document.getElementById("reverse-it");
-reverse_button.addEventListener("click", reverse_handler);
+var reverse_button = document.getElementById('reverse-it');
+reverse_button.addEventListener('click', reverse_handler);
