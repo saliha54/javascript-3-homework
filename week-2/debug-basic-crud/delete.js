ca19bd05157f3caf1929a59ce9005240dcf14b76
saliha54 <51030929+saliha54@users.noreply.github.com>
2019-08-18 10:43:09 +0200

    Update delete.js

--- week-2/debug-basic-crud/delete.js
+++ week-2/debug-basic-crud/delete.js
@@ -1,27 +1,29 @@
 function delete_city_handler() {
   // read user input
-  const user_input_field = document.getElementById("city-to-delete");
+  const user_input_field = document.getElementById('city-to-delete');
   const id_string = user_input_field.value;
   const id_to_delete = Number(id_string);
 
   // perform core logic
-  const deleted_entry = delete cities[id_to_delete];
+  const deleted_entry = cities[id_to_delete];
 
   // show results to user
-  const display_zone = document.getElementById("display-zone");
-  while(display_zone.firstChild){
-      display_zone.removeChild(display_zone.firstChild);
+  const display_zone = document.getElementById('display-zone');
+  var child = display_zone.lastElementChild;
+  while (child) {
+    display_zone.removeChild(child);
+    child = display_zone.lastElementChild;
   }
 
-  const message_p = document.createElement("p");
-  message_p.innerHTML = "you just deleted "+deleted_entry.name;
-
+  const message_p = document.createElement('p');
+  message_p.innerHTML = 'you just deleted ' + deleted_entry.name;
 
   display_zone.appendChild(message_p);
 
-  console.log("you just deleted "+deleted_entry.name);
+  console.log('you just deleted ' + deleted_entry.name);
 
+  delete cities[id_to_delete];
 }
 
-const delete_city_button = document.getElementById("create-city");
-delete_city_button.addEventListener("click", delete_city_handler);
+const delete_city_button = document.getElementById('delete-city');
+delete_city_button.addEventListener('click', delete_city_handler);
