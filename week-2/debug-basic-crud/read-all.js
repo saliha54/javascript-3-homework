d05de2dc3b00d6601a7c70e944e258f32f412cd2
saliha54 <51030929+saliha54@users.noreply.github.com>
2019-08-18 10:42:16 +0200

    Update read-all.js

--- week-2/debug-basic-crud/read-all.js
+++ week-2/debug-basic-crud/read-all.js
@@ -5,28 +5,28 @@ function view_all_handler() {
   // perform core logic
   const cities_entries = [];
   for (let key in cities) {
-    cities.push(cities[key]);
+    cities_entries.push(cities[key]);
   }
-
+  console.log(cities_entries);
   // show results to user
-  const display_zone = document.getElementById("display-zone");
-  while(display_zone.firstChild){
-      display_zone.removeChild(display_zone.firstChild);
+  const display_zone = document.getElementById('display-zone');
+  var child = display_zone.lastElementChild;
+  while (child) {
+    display_zone.removeChild(child);
+    child = display_zone.lastElementChild;
   }
 
-  const cities_ul = document.createElement("ul");
+  const cities_ul = document.createElement('ul');
   for (let city of cities_entries) {
-    const next_li = document.createElement("li");
-    citis_ul.innerHTML = city.id+": "+city.name;
+    const next_li = document.createElement('li');
+    next_li.innerHTML = city.id + ': ' + city.name;
     cities_ul.appendChild(next_li);
   }
 
   display_zone.appendChild(cities_ul);
 
-
-  console.log("you viewed all citites");
-
+  console.log('you viewed all cities');
 }
 
-const view_all_button = document.getElementById("view-all-cities");
-view_all_button.addEventListener("click", view_all_handler);
+const view_all_button = document.getElementById('view-all-cities');
+view_all_button.addEventListener('click', view_all_handler);
